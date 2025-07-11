0cf0c9381fb34252fd4db6fe67e6cb89d950797d
FitFood <alirezamohammadi13878787@gmail.com>
2025-07-11 13:47:28 +0330

    Add files via upload

---
+++ Terms Of Service/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    document.querySelector(".hamburger").classList.toggle("active");
+    document.getElementById("myMenu").classList.toggle("show");
+    document.body.classList.toggle("menu-open");
+  }
